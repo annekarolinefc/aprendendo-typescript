4665c440b761fca0671dc6c45a5744e3ee3684b4
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-06-23 17:54:00 -0300

    Compilador infere o tipo da variável

--- exemplo.ts
+++ exemplo.ts
@@ -2,3 +2,6 @@
 function soma1(a,b){
     return a+b;
 }
+
+//O compilador infere o tipo das variáveis quando possível, caso contrário o tipo é definido implicitamente como 'any'
+const n1 = 4*7/3; //infere como number
