8e5b6fc494ba7852d17a5de3a2caa39df1f56840
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-06-24 15:48:49 -0300

    Criando o backend

--- agenda-react/src/App.tsx
+++ agenda-react/src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { getEventsEndpoint } from "./backend";
+
 
 function App() {
+  /*
+  getEventsEndpoint().then(events => {
+    for(const event of events){
+      console.log(event);
+    }
+  })
+*/
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Olá
     </div>
   );
 }
--- agenda-react/src/backend.tsx
+++ agenda-react/src/backend.tsx
@@ -13,13 +13,15 @@ export interface IEvents {
 }
 
 //FUNÇÕES PARA FAZER CHAMADAS AO BACKEND
-export function getCalendarsEndpoint(): Promise<ICalendar[]> {
+export function getCalendarsEndpoint() {
+//export function getCalendarsEndpoint(): Promise<ICalendar[]> {
     fetch("http://localhost:9090/calendars").then(resp =>{
         return resp.json(); //retorno da promisse
     });
 }
 
-export function getEventsEndpoint(): Promise<IEvents[]> {
+export function getEventsEndpoint(){
+//export function getEventsEndpoint(): Promise<IEvents[]> {
     fetch("http://localhost:9090/calendars").then(resp =>{
         return resp.json(); //retorno da promisse
     });
